6d36e6e72a6342c0b910a502a2bdf35da06fb169
idan157001 <idanalbam1@gmail.com>
2025-08-17 18:51:23 +0300

    changed to production state

--- website/firebase/__init__.py
+++ website/firebase/__init__.py
@@ -1,13 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, db
 import os
-
+import json
 FIREBASE_DB_URL = os.environ.get("FIREBASE_DATABASE_URL")
 FIREBASE_JSON = os.environ.get("FIREBASE_JSON")
 
 def init_firebase():
     if not firebase_admin._apps:  # Prevent re-init
-        cred = credentials.Certificate(FIREBASE_JSON)
+        cred = credentials.Certificate(json.loads(FIREBASE_JSON)) #PRODUCTION-FLAG
         firebase_admin.initialize_app(cred, {"databaseURL": FIREBASE_DB_URL})
     return db
 
